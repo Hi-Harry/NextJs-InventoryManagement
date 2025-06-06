7ea4ee0afe38d3d0b98cb5a80bbb0be4fc080f54
Hi-Harry <haryhary2121@gmail.com>
2025-06-06 14:09:49 +0530

    updating configuration file

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: {
-        name: "inventory-management",
+        name: "NextJs-InventoryManagement",
         scripts: "npm",
         args: "run dev",
         env: {
