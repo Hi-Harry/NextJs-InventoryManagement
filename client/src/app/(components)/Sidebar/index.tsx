ebc115ecdfd21e9944e87a56c5741a29c5d02ef5
Hi-Harry <haryhary2121@gmail.com>
2025-06-07 16:51:49 +0530

    correct image url

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -83,7 +83,7 @@ const Sidebar = () => {
       >
         <Image
           src="https://s3-inventorymanagementdashboard.s3.ap-south-1.amazonaws.com/logo.png"
-          alt="edstock-logo"
+          alt="InventStock-logo"
           width={27}
           height={27}
           className="rounded w-8"
@@ -93,7 +93,7 @@ const Sidebar = () => {
             isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
-          EDSTOCK
+          InventStock
         </h1>
 
         <button
@@ -146,7 +146,7 @@ const Sidebar = () => {
 
       {/* FOOTER */}
       <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
-        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 InventStock</p>
       </div>
     </div>
   );
