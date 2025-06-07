9efcf6e88926c93fec9950c530da037fcc882139
Hi-Harry <haryhary2121@gmail.com>
2025-06-07 16:42:57 +0530

    correcting url for images

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://s3-inventorymanagementdashboard.s3.ap-south-1.amazonaws.com",
+        hostname: "s3-inventorymanagementdashboard.s3.ap-south-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
